be9cb090f268261da1bfef9cc0503dacdf9cdf09
Jirch Tumbokon <jirchgot@gmail.com>
2025-03-04 09:15:41 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_Serialization_And_RAF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
