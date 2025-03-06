c28e06d3eb474d362f247a07b09cc86dabdb5c62
Jirch <jirchgot@gmail.com>
2025-03-06 09:14:58 -0700

    Create serialization and deserialization of event.txt file

--- Program.cs
+++ Program.cs
@@ -19,7 +19,13 @@ namespace Lab_Serialization_And_RAF
     {
         static void Main(string[] args)
         {
-            
+            Event @event1 = new Event(1, "Calgary");
+
+            JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(@event1, options);
+            File.WriteAllText("event.txt", jsonString);
+
+            Event @event2 = JsonSerializer.Deserialize<Event>(jsonString)!;
         }
     }
 }
