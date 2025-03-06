f9981ffde9454c9c1b0683d46295bf7af81856f7
Jirch <jirchgot@gmail.com>
2025-03-06 09:14:27 -0700

    Create Event Class

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Lab_Serialization_And_RAF
+﻿using System.Text.Json;
+
+namespace Lab_Serialization_And_RAF
 {
-    internal class Program
+    public class Event
+    {
+        // public properties
+        public int eventNumber { get; set; }
+        public string location { get; set; }
+
+        public Event(int eventNumber, string location)
+        {
+            this.eventNumber = eventNumber;
+            this.location = location;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
+
